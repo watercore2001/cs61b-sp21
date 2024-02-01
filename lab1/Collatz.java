77d06a0e07be25c1366c7c6465f9d7697c05502c
watercore <watercore2001@gmail.com>
2024-02-01 14:50:21 +0800

    Finish nextNumebr function in lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,24 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Given number n, return 1 if n==1,
+     * return n/2 if n is even,
+     * return 3n+1 if n is odd.
+     *
+     * @param n given number
+     * @return 1 or n/2 or 3n+1
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n  == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
         }
+
+        if (n % 2 == 1) {
+            return 3 * n + 1;
+        }
+
+        return n / 2;
+
     }
 
     public static void main(String[] args) {
